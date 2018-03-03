e4a2f4289da25f3b5ef855bb83cd74f9e69e8a21
Thevaa Chandereng <chandereng@wisc.edu>
2018-03-03 03:48:02 -0600

    included x and y axis label

--- server.R
+++ server.R
@@ -26,7 +26,7 @@ function(input, output) {
   eff <- reactive({
       if(input$margin == "add"){
           data.frame(control = ceiling(h * sum(as.numeric(add.margin(delta = input$marginval, varC = input$varC, varT = input$varT,
-                                                             alpha = as.numeric(input$type1), beta = 1 - as.numeric(input$power)))[1:2])), 
+                                                             alpha = as.numeric(input$type1), beta = 1 - as.numeric(input$power)))[1:2])),
                      eff1 = (input$varC^2 / (h) + input$varT^2/ (1 - h)) / (input$varC + input$varT)^2)
       }
       else{
@@ -35,14 +35,16 @@ function(input, output) {
                      eff1 = (input$varC^2 / h + input$marginval^2 * input$varT^2/ (1 - h)) / (input$varC + input$marginval * input$varT)^2)
       }
   })
-  
+
   # Show the values in an HTML table ----
   output$values <- renderTable({
     sliderValues()
   })
-  
+  f <- list(family = "Courier New, monospace", size = 14, color = "#7f7f7f")
+  x <- list(title = "Sample Size in Control", titlefont = f)
+  y <- list(title = "Relative Efficiency", titlefont = f)
   output$plot <- renderPlotly({
-    plot_ly(eff(), x = ~control, y = ~eff1, type = 'scatter', mode = 'lines')
+    plot_ly(eff(), x = ~control, y = ~eff1, type = 'scatter', mode = 'lines') %>%
+      layout(title = "Relative Efficiency of Sample size in Control vs Optimal Allocation", xaxis = x, yaxis = y)
   })
-  
 }
