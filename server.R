f4b0f66c7b5f904a92a7657b944153e3f0482207
Thevaa Chandereng <chandereng@wisc.edu>
2018-03-03 03:05:48 -0600

    added ceiling values and rounded output

--- server.R
+++ server.R
@@ -5,31 +5,33 @@ h  <- seq(0.05, 0.95, 0.01)
 function(input, output) {
   sliderValues <- reactive({
     if(input$margin == "add"){
+      val <- add.margin(delta = input$marginval, varC = input$varC, varT = input$varT,
+                 alpha = as.numeric(input$type1), beta = 1 - as.numeric(input$power))
       data.frame(Output = c("Sample Size in Control",
                             "Sample Size in Treatment",
                             "Randomization treatment to control (k:1)",
                             "Realtive Efficiency (compared to 1:1)"),
-      Values =  as.numeric(add.margin(delta = input$marginval, varC = input$varC, varT = input$varT,
-                 alpha = as.numeric(input$type1), beta = 1 - as.numeric(input$power))))
+      Values =  c(ceiling(val$control), ceiling(val$treatment), val$randomization, val$efficiency))
     }
     else{
+      val <- multi.margin(delta = input$marginval, muC = input$muC, varC = input$varC, varT = input$varT,
+                          alpha = as.numeric(input$type1), beta = 1 - as.numeric(input$power))
       data.frame(Output = c("Sample Size in Control",
                             "Sample Size in Treatment",
                             "Randomization treatment to control (k:1)",
                             "Realtive Efficiency (compared to 1:1)"),
-                 Values = as.numeric(multi.margin(delta = input$marginval, muC = input$muC, varC = input$varC, varT = input$varT,
-                 alpha = as.numeric(input$type1), beta = 1 - as.numeric(input$power))))
+                 Values = c(ceiling(val$control), ceiling(val$treatment), val$randomization, val$efficiency))
     }
   })
   eff <- reactive({
       if(input$margin == "add"){
-          data.frame(control = h * sum(as.numeric(add.margin(delta = input$marginval, varC = input$varC, varT = input$varT,
-                                                             alpha = as.numeric(input$type1), beta = 1 - as.numeric(input$power)))[1:2]), 
+          data.frame(control = ceiling(h * sum(as.numeric(add.margin(delta = input$marginval, varC = input$varC, varT = input$varT,
+                                                             alpha = as.numeric(input$type1), beta = 1 - as.numeric(input$power)))[1:2])), 
                      eff1 = (input$varC^2 / (h) + input$varT^2/ (1 - h)) / (input$varC + input$varT)^2)
       }
       else{
-          data.frame(control = h * sum(as.numeric(multi.margin(delta = input$marginval, muC = input$muC, varC = input$varC, varT = input$varT,
-                                                               alpha = as.numeric(input$type1), beta = 1 - as.numeric(input$power)))[1:2]),
+          data.frame(control = ceiling(h * sum(as.numeric(multi.margin(delta = input$marginval, muC = input$muC, varC = input$varC, varT = input$varT,
+                                                               alpha = as.numeric(input$type1), beta = 1 - as.numeric(input$power)))[1:2])),
                      eff1 = (input$varC^2 / h + input$marginval^2 * input$varT^2/ (1 - h)) / (input$varC + input$marginval * input$varT)^2)
       }
   })
